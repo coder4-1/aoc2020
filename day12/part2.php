b31309835fc06b9fa5bdc51219aa79660edc309e
Marcel <>
2020-12-12 21:36:21 +0100

    Day 12 Part 2

---
+++ day12/part2.php
@@ -0,0 +1,57 @@
+<?php
+$path="input";
+$lines=file($path);
+
+$xWaypoint = 10;
+$yWaypoint = -1;
+$xShip = 0;
+$yShip = 0;
+
+foreach($lines as $line) {
+    $command = substr($line,0,1);
+    $arg = trim(substr($line,1));
+    if ($command == "N") {
+        $yWaypoint -= $arg;
+    } else if ($command == "E") {
+        $xWaypoint += $arg;
+    } else if ($command == "S") {
+        $yWaypoint += $arg;
+    } else if ($command == "W") {
+        $xWaypoint -= $arg;
+    } else if ($command == "L") {
+        $x=$xWaypoint;
+        $y=$yWaypoint;
+        if ($arg == 90) {
+            $xWaypoint = $y;
+            $yWaypoint = -$x;
+        } else if ($arg == 180) {
+            $xWaypoint = -$x;
+            $yWaypoint = -$y;
+        } else if ($arg == 270) {
+            $xWaypoint = -$y;
+            $yWaypoint = $x;
+        }
+    } else if ($command == "R") {
+        $x=$xWaypoint;
+        $y=$yWaypoint;
+        if ($arg == 90) {
+            $xWaypoint = -$y;
+            $yWaypoint = $x;
+        } else if ($arg == 180) {
+            $xWaypoint = -$x;
+            $yWaypoint = -$y;
+        } else if ($arg == 270) {
+            $xWaypoint = $y;
+            $yWaypoint = -$x;
+        }
+    } else if ($command == "F") {
+        $xShip = $xShip + $xWaypoint * $arg;
+        $yShip = $yShip + $yWaypoint * $arg;
+    }
+    //echo "xWaypoint:$xWaypoint yWaypoint:$yWaypoint xShip:$xShip yShip:$yShip<br>";
+}
+
+$sum = abs($xShip) + abs($yShip);
+echo "Sum of x und y: $sum<br>";
+
+// Too low: 32682
